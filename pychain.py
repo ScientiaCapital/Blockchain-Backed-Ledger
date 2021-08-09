4cfd656d6360ff6dc8186df7f465178f1cb0dbfe
Tim Kipper <tkipper@gmail.com>
2021-08-09 10:56:16 -0500

    Update code

--- pychain.py
+++ pychain.py
@@ -49,7 +49,12 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
+@dataclass
+class Record:
+
+    sender: str
+    receiver: str
+    amount: float
 
 
 ################################################################################
@@ -66,9 +71,9 @@ import hashlib
 @dataclass
 class Block:
 
-    # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record
+data: Any
 
     creator_id: int
     prev_hash: str = 0
@@ -170,7 +175,7 @@ input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender_input = st.text_input("Sender")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
